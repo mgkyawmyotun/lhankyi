6fbcda0ea39a18ab0ff22ca91acde19215e8d40d
kyawmyotun <mg.kmt.kyawmyotun@gmail.com>
2021-10-19 10:40:25 +0630

    fix: parser limit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
